05bb08a437d542683538bc8ad8e542bb45e3d9c2
Sharon Awuor <sharonawuor@gmail.com>
2026-06-26 00:04:39 +0300

    Progress 3 done

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,5 +1,7 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -7,8 +9,45 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionListener {
 
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        System.out.println(transaction);
+
+        UserRecord sender =
+                databaseConduit.findUser(transaction.getSenderId());
+
+        UserRecord recipient =
+                databaseConduit.findUser(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) {
+            return;
+        }
+
+        if (sender.getBalance() < transaction.getAmount()) {
+            return;
+        }
+
+        sender.setBalance(
+                sender.getBalance() - transaction.getAmount());
+
+        recipient.setBalance(
+                recipient.getBalance() + transaction.getAmount());
+
+        databaseConduit.save(sender);
+        databaseConduit.save(recipient);
+
+        databaseConduit.saveTransaction(
+                new TransactionRecord(
+                        sender,
+                        recipient,
+                        transaction.getAmount()
+                )
+        );
+        System.out.println("Processing: " + transaction);
     }
 }
